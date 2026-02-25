618e277bee9e3944a0ad86d7567fa7e83afbbee9
janhae4 <hlchy11997@gmail.com>
2026-02-25 14:25:05 +0700

    fix docker set up

--- backend/apps/socket/src/socket.module.ts
+++ backend/apps/socket/src/socket.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SocketController } from './socket.controller';
 import { SocketGateway } from './socket.gateway';
-import { ClientConfigModule} from '@app/contracts';
+import { ChatGateway } from './chat.gateway';
+import { VoiceGateway } from './voice.gateway';
+import { ClientConfigModule } from '@app/contracts';
 import { RmqModule } from '@app/common';
+import { RedisServiceModule } from '@app/redis-service';
 
 @Module({
   imports: [
     ClientConfigModule,
-    RmqModule.register()
+    RmqModule.register(),
+    RedisServiceModule
   ],
   controllers: [SocketController],
-  providers: [SocketGateway],
+  providers: [SocketGateway, ChatGateway, VoiceGateway],
 })
 export class SocketModule { }
